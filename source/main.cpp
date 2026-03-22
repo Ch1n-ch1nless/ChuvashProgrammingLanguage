efbe61f83bc0574f0add56b490a689de24d5908e
Ch1n-ch1nless <aleksandrov.oa.work@gmail.com>
2026-03-22 23:53:26 +0300

    Start

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <parser/parser.hpp>
+#include <parser/print_ast.hpp>
 #include <token/to_string.hpp>
 #include <token/tokenizer.hpp>
 
 int main() {
+  // Current simple programm
   std::string text = "func main() { ret 52 }";
+
+  // Execute tokenization stage
+  std::cout << "Result of tokenization:\n";
+  std::cout << "==============================\n";
+
   auto tokens = token::tokenize(text);
   if (tokens.has_value()) {
     for (const auto& token : *tokens) {
@@ -12,11 +19,23 @@ int main() {
                 << token.beginPos.column
                 << ") = " << token::toString(token.token) << "\n";
     }
-
-    auto parsingResult = parser::parse(*tokens);
   } else {
     std::cerr << "Tokenization error: " << tokens.error() << std::endl;
     return 1;
   }
+  std::cout << "==============================\n\n";
+
+  // Execute parsing stage:
+  std::cout << "Result of parsing\n";
+  std::cout << "==============================\n";
+
+  auto parsingResult = parser::parse(*tokens);
+  if (parsingResult.has_value()) {
+    parser::printAST(*parsingResult);
+  } else {
+    std::cout << parsingResult.error();
+  }
+  std::cout << "==============================\n\n";
+
   return 0;
 }
